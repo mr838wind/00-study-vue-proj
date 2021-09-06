ac0922e4cba5aa403966de3f59d3653ad05c3dee
chhan <chhan@wdfall.com>
2021-09-06 10:58:36 +0900

    [study] env

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@
 import {BoardService, MockBoardService} from "@/services/BoardService";
 
 const API_URL_HOST = process.env.VUE_APP_API_HOST;
+console.log('>>== API_URL_HOST=%o', API_URL_HOST);
 
 
 const providedServices = {
