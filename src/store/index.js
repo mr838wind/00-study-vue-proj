db635cac175704c7dd55e4546658dbb4b8acd445
mr838wind <mr838wind@gmail.com>
2021-09-06 23:07:56 +0900

    [study] store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import account from './modules/account'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     account: account,
-  }
+  },
+  plugins: [
+    createPersistedState({
+      paths: ["account"],
+    })
+  ]
 })
