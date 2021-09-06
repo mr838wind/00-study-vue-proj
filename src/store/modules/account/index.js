6d8ee75b7c527da0e4005d9b0fc9cf14b2c513f4
mr838wind <mr838wind@gmail.com>
2021-09-06 22:52:06 +0900

    [study] store

--- src/store/modules/account/index.js
+++ src/store/modules/account/index.js
@@ -1,22 +1,16 @@
 
-
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
 const state = {
     name: 'default'
-}
-
-const getters = {
-
 }
 
 const mutations ={
     setName(state, name) {
         state.name = name;
     }
+}
+
+const getters = {
+
 }
 
 const actions = {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,49 @@
 <template>
   <div class="home">
       <div>
-          test:
+          test: {{name}} <br/>
+          {{accountName}}
       </div>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div>
+        <input v-model="localName" />
+        <button @click="onClickChange">change name</button>
+      </div>
+
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import {mapMutations, mapState} from "vuex";
 
 export default {
-  name: 'Home',
-  inject:['boardService'],
-  components: {
-    HelloWorld
-  },
+    name: 'Home',
+    inject:['boardService'],
+    components: {
+      // eslint-disable-next-line vue/no-unused-components
+      HelloWorld
+    },
+    data() {
+      return {
+        localName: '',
+      }
+    },
     computed: {
+      ...mapState('account', ['name']),
+      ...mapState('account', { accountName: state => state.name}),
     },
     async created() {
       console.log('>>== created');
       //const resp = await this.boardService.getBoardList('aa','');
-
-
+    },
+    methods: {
+      ...mapMutations('account', ['setName']),
+      onClickChange() {
+        this.setName(this.localName);
+      }
     }
 
 }
