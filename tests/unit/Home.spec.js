ae109300e546f90fa3a166e779aaf59945f74f20
mr838wind <mr838wind@gmail.com>
2021-09-07 23:09:07 +0900

    [study] test: with provide, store

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,21 +1,21 @@
-import Vue from 'vue';
 import { shallowMount } from '@vue/test-utils';
 import Home from '@/views/Home.vue';
+import providedService from "@/services";
+import store from '@/store';
 
 /**
  * hard to test:
- * TODO//fix: inject, store ....
  */
 
 describe('Home.vue', () => {
     test('컴포넌트가 마운팅되면 username이 존재하고 초기값으로 설정되어 있어야 한다.', () => {
+        // provide, store 사용
         const wrapper = shallowMount(Home, {
             provide: {
-                boardService() {
-                    return ''
-                }
-            }
+                ...providedService
+            },
+            store,
         });
-        expect(wrapper.vm.localName).toBe('');
+        expect(wrapper.vm.localName).toBe('test');
     });
 });
